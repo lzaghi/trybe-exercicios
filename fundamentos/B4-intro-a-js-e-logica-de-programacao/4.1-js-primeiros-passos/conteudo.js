e7694139653b96f36347ac1e62d94d6cc4e84926
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-07-20 16:06:43 -0300

    exercicio tipos e operadores

--- fundamentos/B4-intro-a-js-e-logica-de-programacao/4.1-js-primeiros-passos/conteudo.js
+++ fundamentos/B4-intro-a-js-e-logica-de-programacao/4.1-js-primeiros-passos/conteudo.js
@@ -1,10 +1,41 @@
-const myName = "Lucca"
-const birthCity = "Guaranésia"
-let birthYear = "1997"
+// VARIAVEIS
+// const myName = "Lucca"
+// const birthCity = "Guaranésia"
+// let birthYear = "1997"
 
-birthYear = 2030
-birthCity = "Guaxupé"
+// birthYear = 2030
+// birthCity = "Guaxupé"
 
-console.log(myName)
-console.log(birthCity)
-console.log(birthYear)
+// console.log(myName)
+// console.log(birthCity)
+// console.log(birthYear)
+
+// TIPOS PRIMITIVOS E OPERADORES
+// let patientId = '50';
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof patientId)
+// console.log(typeof isEnrolled)
+// console.log(typeof patientInfo)
+// console.log(typeof patientEmail)
+
+// console.log(typeof patientAge)
+
+
+// const base = 5;
+// const height = 8;
+
+// const area = base * height;
+
+// console.log(base)
+// console.log(height)
+// console.log(area)
+
+// const perimeter = (base*2) + (height*2);
+
+// console.log(perimeter)
