71cfe015abd246cdf6a42af37fddb0deaa30a74d
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-07-20 16:17:17 -0300

    exercicio operadores logicos

--- fundamentos/B4-intro-a-js-e-logica-de-programacao/4.1-js-primeiros-passos/conteudo.js
+++ fundamentos/B4-intro-a-js-e-logica-de-programacao/4.1-js-primeiros-passos/conteudo.js
@@ -39,3 +39,14 @@
 // const perimeter = (base*2) + (height*2);
 
 // console.log(perimeter)
+
+// OPERADORES LÓGICOS
+// const nota = 2
+
+// if (nota >= 80) {
+//   console.log("Parabéns, você foi aprovada(o)!")
+// } else if (nota < 80 && nota >= 60) {
+//   console.log("Você está na nossa lista de espera :)")
+// } else if (nota < 60) {
+//   console.log("Você foi reprovada(o) :(")
+// }
