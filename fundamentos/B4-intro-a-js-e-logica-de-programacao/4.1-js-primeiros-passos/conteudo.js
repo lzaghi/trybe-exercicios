847a12c6581dd74f3cba2390fdc56473a87ca70f
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-07-20 15:45:46 -0300

    exercicio variaveis

---
+++ fundamentos/B4-intro-a-js-e-logica-de-programacao/4.1-js-primeiros-passos/conteudo.js
@@ -0,0 +1,10 @@
+const myName = "Lucca"
+const birthCity = "Guaranésia"
+let birthYear = "1997"
+
+birthYear = 2030
+birthCity = "Guaxupé"
+
+console.log(myName)
+console.log(birthCity)
+console.log(birthYear)
