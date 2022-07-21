d57c017e1f8f585123a16dbbe85e61174a1f0071
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-07-21 15:16:32 -0300

    exercicios array

---
+++ fundamentos/B4-intro-a-js-e-logica-de-programacao/4.2-js-array-e-loop-for/conteudo.js
@@ -0,0 +1,21 @@
+//                                ARRAYS
+
+// EXERCICIO 1
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+
+// EXERCICIO 2
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio') ;
+
+// console.log(indexOfPortfolio);
+
+// EXERCÍCIOS 3
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// menu.push('Contato')
+
+// console.log(menu);
+
+//                                 FOR
