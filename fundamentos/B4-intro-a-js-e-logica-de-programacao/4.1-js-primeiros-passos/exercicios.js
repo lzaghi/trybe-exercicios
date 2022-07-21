f3344133b47eca5352aedf0cdd33823b343b5640
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-07-20 22:09:51 -0300

    exercicios 1 2 e 3

---
+++ fundamentos/B4-intro-a-js-e-logica-de-programacao/4.1-js-primeiros-passos/exercicios.js
@@ -0,0 +1,42 @@
+//                                  EXERCÍCIO 1
+// const a = 20;
+// const b = 40;
+
+// let adicao = a + b;
+// let subtracao = a - b;
+// let multiplicacao = a * b;
+// let divisao = a / b;
+// let modulo = a % b;
+
+// console.log(adicao);
+// console.log(subtracao);
+// console.log(multiplicacao);
+// console.log(divisao);
+// console.log(modulo);
+
+//                                  EXERCICIO 2
+// const a = 80;
+// const b = 50;
+
+// if (a > b) {
+//   console.log(a);
+// } else {
+//   console.log(b);
+// }
+
+//                                  EXERCICIO 3
+// const a = 80;
+// const b = 90;
+// const c = 120;
+
+// if (a > b && a > c) {
+//   console.log(a);
+// } 
+// else if (b > a && b > c) {
+//   console.log(b);
+// }
+// else {
+//   console.log(c);
+// }
+
+//                                 EXERCICIO 4
