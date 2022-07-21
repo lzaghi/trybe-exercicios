9e170d4a50c6aa627cf39d432fe833fb8ea0bdb2
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-07-20 22:35:36 -0300

    exercicios 4 5 e 6

--- fundamentos/B4-intro-a-js-e-logica-de-programacao/4.1-js-primeiros-passos/exercicios.js
+++ fundamentos/B4-intro-a-js-e-logica-de-programacao/4.1-js-primeiros-passos/exercicios.js
@@ -40,3 +40,57 @@
 // }
 
 //                                 EXERCICIO 4
+// const x = 0;
+
+// if (x > 0) {
+//   console.log("positive");
+// }
+// else if (x < 0) {
+//   console.log("negative");
+// }
+// else {
+//   console.log("zero");
+// }
+
+//                               EXERCICIO 5
+// const a = 50;
+// const b = 60;
+// const c = 70;
+
+// if (a < 0 || b < 0 || c < 0) {
+//   console.log("Erro: ângulo inválido");
+// }
+// else if (a + b + c === 180) {
+//   console.log("true");
+// }
+// else if (a + b + c !== 180) {
+//   console.log("false");
+// }
+
+//                              EXERCICIO 6
+// peão torre bispo cavalo rainha rei 
+// const peca = "REi";
+
+// if (peca.toLowerCase() === "peão") {
+//   console.log("Vertical");
+// }
+// else if (peca.toLowerCase() === "torre") {
+//   console.log("Horizontal e Vertical");
+// }
+// else if (peca.toLowerCase() === "bispo") {
+//   console.log("Diagonais");
+// }
+// else if (peca.toLowerCase() === "cavalo") {
+//   console.log("Formato L");
+// }
+// else if (peca.toLowerCase() === "rainha") {
+//   console.log("Horizontal, Vertical e Diagonais");
+// }
+// else if (peca.toLowerCase() === "rei") {
+//   console.log("Horizontal, Vertical e Diagonais");
+// }
+// else {
+//   console.log("Peça inválida");
+// }
+
+//                             EXERCICIO 7
