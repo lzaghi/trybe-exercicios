b7198e0f319e6b36d01ed0087a67c73b95924b3f
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-07-22 19:43:38 -0300

    exercicio 3

--- fundamentos/B4-intro-a-js-e-logica-de-programacao/4.3-js-logica-de-programacao-e-algoritmos/exercicios.js
+++ fundamentos/B4-intro-a-js-e-logica-de-programacao/4.3-js-logica-de-programacao-e-algoritmos/exercicios.js
@@ -17,12 +17,40 @@
 
 
 //                                  EXERCICIO 2
-let word = 'socorram me subi no onibus em marrocos';
+// let word = 'socorram me subi no onibus em marrocos';
 
-let novaWord = '';
+// let novaWord = '';
 
-for (let i = word.length - 1; i >= 0; i -= 1) {
-  novaWord += word[i];
+// for (let i = word.length - 1; i >= 0; i -= 1) {
+//   novaWord += word[i];
+// }
+
+// console.log(novaWord);
+
+
+//                                 EXERCICIO 3
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let arrayTamanhos =[];
+for (let i = 0; i < array.length; i += 1) {
+  arrayTamanhos.push(array[i].length)
 }
 
-console.log(novaWord);
+let maior = arrayTamanhos[0]
+let index;
+for (let j = 1; j < arrayTamanhos.length; j += 1) {
+  if (arrayTamanhos[j] > maior) {
+    maior = arrayTamanhos[j];
+    index = j;
+  }
+}
+console.log("a maior palavra é:", array[index], "com", maior, "caracteres");
+
+let menor = arrayTamanhos[0]
+let index2;
+for (let k = 1; k < arrayTamanhos.length; k += 1) {
+  if (arrayTamanhos[k] < menor) {
+    menor = arrayTamanhos[k];
+    index2 = k;
+  }
+}
+console.log("a menor palavra é:", array[index2], "com", menor, "caracteres");
