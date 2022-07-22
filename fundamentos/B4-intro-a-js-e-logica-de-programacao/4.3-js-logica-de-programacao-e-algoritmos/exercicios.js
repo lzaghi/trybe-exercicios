6d14e9e9e62bb52cca65d72cad91edc430221c54
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-07-22 18:57:11 -0300

    exercicio 2

--- fundamentos/B4-intro-a-js-e-logica-de-programacao/4.3-js-logica-de-programacao-e-algoritmos/exercicios.js
+++ fundamentos/B4-intro-a-js-e-logica-de-programacao/4.3-js-logica-de-programacao-e-algoritmos/exercicios.js
@@ -1,16 +1,28 @@
 
-// EXERCICIO 1
-let number = 10;
-let array = [];
+//                                   EXERCICIO 1
+// let number = 10;
+// let array = [];
 
-for (let i = 1; i <= number; i += 1) {
-  array.push(i)
+// for (let i = 1; i <= number; i += 1) {
+//   array.push(i)
+// }
+
+// let total = 1;
+
+// for (let j = 0; j < array.length ; j += 1) {
+//   total *= array[j]
+// }
+
+// console.log(total);
+
+
+//                                  EXERCICIO 2
+let word = 'socorram me subi no onibus em marrocos';
+
+let novaWord = '';
+
+for (let i = word.length - 1; i >= 0; i -= 1) {
+  novaWord += word[i];
 }
 
-let total = 1;
-
-for (let j = 0; j < array.length ; j += 1) {
-  total *= array[j]
-}
-
-console.log(total);
+console.log(novaWord);
