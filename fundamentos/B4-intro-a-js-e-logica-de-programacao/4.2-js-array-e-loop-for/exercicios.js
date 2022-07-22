c38dce6c726a45a053ea7168525c58ef02ad9b8d
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-07-21 23:10:53 -0300

    exercicio bonus 3

--- fundamentos/B4-intro-a-js-e-logica-de-programacao/4.2-js-array-e-loop-for/exercicios.js
+++ fundamentos/B4-intro-a-js-e-logica-de-programacao/4.2-js-array-e-loop-for/exercicios.js
@@ -132,17 +132,34 @@
 
 
 //              EXERCICIO BONUS 2
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let i = 1; i < numbers.length; i += 1) {
+
+//   for (let j = 0; j < i; j += 1) {
+
+//     if (numbers[i] > numbers[j]) {
+//       let position = numbers[i];
+//       numbers[i] = numbers[j];
+//       numbers[j] = position;
+//     }
+//   }
+// }
+// console.log(numbers);
+
+
+//              EXERICIO BONUS 3
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let novo = [];
 
-for (let i = 1; i < numbers.length; i += 1) {
+for (let i = 0; i < numbers.length; i += 1) {
 
-  for (let j = 0; j < i; j += 1) {
-
-    if (numbers[i] > numbers[j]) {
-      let position = numbers[i];
-      numbers[i] = numbers[j];
-      numbers[j] = position;
-    }
+  if (i === numbers.length - 1) {
+    novo.push(numbers[i]*2)
+  }
+  else {
+    novo.push(numbers[i]*numbers[i+1])
   }
 }
-console.log(numbers);
+
+console.log(novo);
