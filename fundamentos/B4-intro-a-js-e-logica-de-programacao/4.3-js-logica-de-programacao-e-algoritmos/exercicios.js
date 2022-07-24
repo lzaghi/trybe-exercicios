aca1cdc0115ec20f2425b3359b7b774aa1ecea83
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-07-24 10:54:52 -0300

    exercicio bonus 2

--- fundamentos/B4-intro-a-js-e-logica-de-programacao/4.3-js-logica-de-programacao-e-algoritmos/exercicios.js
+++ fundamentos/B4-intro-a-js-e-logica-de-programacao/4.3-js-logica-de-programacao-e-algoritmos/exercicios.js
@@ -78,13 +78,23 @@
 
 
 //                            EXERCICIO BONUS 1
+// let n = 5;
+// let string = "";
+
+// for (let i = 1; i <= n; i += 1) {
+//   string += "*"
+// }
+
+// for (j = 0; j < n; j+= 1){
+//   console.log(string);
+// }
+
+
+//                          EXERCICIO BONUS 2
 let n = 5;
 let string = "";
 
-for (let i = 1; i <= n; i += 1) {
-  string += "*"
-}
-
-for (j = 0; j < n; j+= 1){
+for (let i = 0; i < n; i += 1){
+  string += "*";
   console.log(string);
 }
