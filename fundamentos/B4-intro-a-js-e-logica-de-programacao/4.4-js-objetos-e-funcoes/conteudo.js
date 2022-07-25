26faa2724059f397eda394115b862fd242abda10
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-07-25 18:45:03 -0300

    exercicios for/of for/in

--- fundamentos/B4-intro-a-js-e-logica-de-programacao/4.4-js-objetos-e-funcoes/conteudo.js
+++ fundamentos/B4-intro-a-js-e-logica-de-programacao/4.4-js-objetos-e-funcoes/conteudo.js
@@ -152,22 +152,47 @@
 //==========OBJETOS============
 
 //            EXERCICIO 1
-let player = {
-  name: "Marta",
-  lastName: "Silva",
-  age: 34,
-  medals: { golden: 2, silver: 3, },
+// let player = {
+//   name: "Marta",
+//   lastName: "Silva",
+//   age: 34,
+//   medals: { golden: 2, silver: 3, },
+// }
+
+// //           EXERCICIO 2
+// console.log("A jogadora", player.name, player.lastName, "tem", player.age, "anos de idade");
+
+// //           EXERCICIO 3
+// player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018]
+// console.log(player);
+
+// //           EXERCICIO 4
+// console.log("A jogadora ", player.name, player.lastName, "foi eleita a melhor do mundo por", player.bestInTheWorld.length, "vezes");
+
+// //          EXERCICIO 5
+// console.log("A jogadora possui", player.medals.golden, "medalhas de ouro e", player.medals.silver, "medalhas de prata");
+
+
+// ================FOR/IN FOR/OF
+
+//            EXERCICIO 1
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge',
+};
+
+for (let keys in names) {
+  console.log("Olá", names[keys]);
 }
 
 //           EXERCICIO 2
-console.log("A jogadora", player.name, player.lastName, "tem", player.age, "anos de idade");
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
 
-//           EXERCICIO 3
-player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018]
-console.log(player);
-
-//           EXERCICIO 4
-console.log("A jogadora ", player.name, player.lastName, "foi eleita a melhor do mundo por", player.bestInTheWorld.length, "vezes");
-
-//          EXERCICIO 5
-console.log("A jogadora possui", player.medals.golden, "medalhas de ouro e", player.medals.silver, "medalhas de prata");
+for (let keys in car) {
+  console.log(keys, ":", car[keys]);
+}
