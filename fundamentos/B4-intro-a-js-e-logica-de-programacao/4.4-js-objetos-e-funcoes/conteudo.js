73565845c4db39cf2e4dcc97fa737a45dc7cd139
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-07-25 18:36:44 -0300

    exercicios objetos

--- fundamentos/B4-intro-a-js-e-logica-de-programacao/4.4-js-objetos-e-funcoes/conteudo.js
+++ fundamentos/B4-intro-a-js-e-logica-de-programacao/4.4-js-objetos-e-funcoes/conteudo.js
@@ -148,3 +148,26 @@
 // console.log(maiorNum(10, 20)); // 20 é maior que 10
 // console.log(maiorNum(2, -5)); // 2 é maior que -5
 // console.log(maiorNum(1, 1)); // Os números são iguais
+
+//==========OBJETOS============
+
+//            EXERCICIO 1
+let player = {
+  name: "Marta",
+  lastName: "Silva",
+  age: 34,
+  medals: { golden: 2, silver: 3, },
+}
+
+//           EXERCICIO 2
+console.log("A jogadora", player.name, player.lastName, "tem", player.age, "anos de idade");
+
+//           EXERCICIO 3
+player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018]
+console.log(player);
+
+//           EXERCICIO 4
+console.log("A jogadora ", player.name, player.lastName, "foi eleita a melhor do mundo por", player.bestInTheWorld.length, "vezes");
+
+//          EXERCICIO 5
+console.log("A jogadora possui", player.medals.golden, "medalhas de ouro e", player.medals.silver, "medalhas de prata");
