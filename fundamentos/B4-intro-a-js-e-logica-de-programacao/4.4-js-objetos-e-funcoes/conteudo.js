1b22af59eb30b8604dc7f9b98a78702a7993cba4
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-07-25 16:31:34 -0300

    exemplos ao longo do conteudo

---
+++ fundamentos/B4-intro-a-js-e-logica-de-programacao/4.4-js-objetos-e-funcoes/conteudo.js
@@ -0,0 +1,150 @@
+// let diasDaSemana = {
+//   1: 'domingo',
+//   2: 'segunda',
+//   3: 'terça',
+//   4: 'quarta',
+//   5: 'quinta',
+//   6: 'sexta',
+//   7: 'sábado',
+//   };
+  
+//   // diasDaSemana.1; // SyntaxError: Unexpected number
+//   console.log(diasDaSemana['1']); // domingo
+
+
+// let conta = {
+//   agencia: '0000',
+//   banco: {
+//     cod: '012',
+//     id: 4,
+//     nome: 'TrybeBank',
+//   },
+// };
+
+// let infoDoBanco = 'banco';
+// console.log(conta[infoDoBanco]); // { cod: '012', id: 4, nome: 'TrybeBank' }
+// console.log(conta[infoDoBanco]['nome']); // TrybeBank
+
+// console.log(conta.agencia); // 0000
+// console.log(conta['agencia']); // 0000
+
+// console.log(conta.banco.cod); // 012
+// console.log(conta['banco']['id']); // 4
+
+
+// let usuario = {
+//   id: 99,
+//   email: 'jakeperalta@gmail.com',
+//   infoPessoal: {
+//     nome: 'Jake',
+//     sobrenome: 'Peralta',
+//     endereco: {
+//       rua: 'Smith Street',
+//       bairro: 'Brooklyn',
+//       cidade: 'Nova Iorque',
+//       estado: 'Nova Iorque',
+//     },
+//   },
+// };
+
+// console.log(usuario['id']); // 99
+// console.log(usuario.email); // jakeperalta@gmail.com
+
+// console.log(usuario.infoPessoal.endereco.rua); // Smith Street
+// console.log(usuario['infoPessoal']['endereco']['cidade']); // Nova Iorque
+
+
+// let moradores = [
+//   {
+//     nome: 'Luiza',
+//     sobrenome: 'Guimarães',
+//     andar: 10,
+//     apartamento: 1005,
+//   },
+//   {
+//     nome: 'William',
+//     sobrenome: 'Albuquerque',
+//     andar: 5,
+//     apartamento: 502,
+//   },
+//   {
+//     nome: 'Murilo',
+//     sobrenome: 'Ferraz',
+//     andar: 8,
+//     apartamento: 804,
+//   },
+//   {
+//     nome: 'Zoey',
+//     sobrenome: 'Brooks',
+//     andar: 1,
+//     apartamento: 101,
+//   },
+// ];
+
+// let primeiroMorador = moradores[0];
+// console.log(primeiroMorador); // { nome: 'Luiza', sobrenome: 'Guimarães', andar: 10, apartamento: 1005 }
+// console.log(primeiroMorador['andar']); // 10
+
+// let ultimoMorador = moradores[moradores.length - 1];
+// console.log(ultimoMorador); // { nome: 'Zoey', sobrenome: 'Brooks', andar: 1, apartamento: 101 }
+// console.log(ultimoMorador.nome); // Zoey
+
+//==============================
+
+// let car = {
+//   type: 'Fiat',
+//   model: '500',
+//   color: 'white',
+// };
+
+// for (let index in car) {
+//   console.log(index, car[index]);
+// }
+
+// let food = ['hamburguer', 'bife', 'acarajé'];
+// for (let position in food) {
+//   console.log(position);
+// };
+// //resultado: 0, 1, 2;
+
+// let foods = ['hamburguer', 'bife', 'acarajé'];
+// for (let value of foods) {
+//   console.log(value);
+// };
+// //resultado: hamburguer, bife, acarajé;
+
+
+// let status = 'deslogado';
+
+// function logarDeslogar() {
+//   if (status === 'deslogado') {
+//     status = 'logado';
+//   } else {
+//     status = 'deslogado';
+//   }
+// }
+
+// console.log(status); // deslogado
+
+// logarDeslogar();
+// console.log(status); // logado
+
+// logarDeslogar();
+// console.log(status); // deslogado
+
+// console.log('O usuário está ' + status); // O usuário está deslogado
+
+
+// function maiorNum(primeiroNum, segundoNum) {
+//   if (primeiroNum > segundoNum) {
+//     return primeiroNum + ' é maior que ' + segundoNum;
+//   } else if (segundoNum > primeiroNum) {
+//     return segundoNum + ' é maior que ' + primeiroNum;
+//   } else {
+//     return 'Os números são iguais';
+//   }
+// }
+
+// console.log(maiorNum(10, 20)); // 20 é maior que 10
+// console.log(maiorNum(2, -5)); // 2 é maior que -5
+// console.log(maiorNum(1, 1)); // Os números são iguais
