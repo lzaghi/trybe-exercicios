7a7b0d8274b3f42ddcd1fbd45712146fb244545b
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-08-07 11:24:29 -0300

    validação via bibliotecaaa (JustValidate)

--- fundamentos/B6-html-css-forms-flexbox-e-responsivo/6.2-bibliotecas-js-e-frameworks-css/script.js
+++ fundamentos/B6-html-css-forms-flexbox-e-responsivo/6.2-bibliotecas-js-e-frameworks-css/script.js
@@ -1,31 +1,31 @@
-function inputValidation() {
-  const nome = document.getElementById('nome-completo').value.length;
-  const email = document.getElementById('e-mail').value.length;
-  const texto = document.getElementById('text').value.length;
+// function inputValidation() {
+//   const nome = document.getElementById('nome-completo').value.length;
+//   const email = document.getElementById('e-mail').value.length;
+//   const texto = document.getElementById('text').value.length;
 
-  const invalidName = nome < 10 || nome > 40
-  const invalidEmail = email < 10 || email > 50
-  const invalidText = texto > 500
+//   const invalidName = nome < 10 || nome > 40
+//   const invalidEmail = email < 10 || email > 50
+//   const invalidText = texto > 500
 
-  if (invalidName || invalidEmail || invalidText) {
-    return false
-  }
-  else {
-    return true
-  }
-}
+//   if (invalidName || invalidEmail || invalidText) {
+//     return false
+//   }
+//   else {
+//     return true
+//   }
+// }
 
-function handleSubmit(event) {
-  event.preventDefault()
+// function handleSubmit(event) {
+//   event.preventDefault()
 
-  const valid = inputValidation();
-  if (valid === false) {
-    alert('Dados inválidos')
-  }
-  else {
-    alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip')
-  }
-}
+//   const valid = inputValidation();
+//   if (valid === false) {
+//     alert('Dados inválidos')
+//   }
+//   else {
+//     alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip')
+//   }
+// }
 
 function enableSubmit() {
   const botaoSubmit = document.getElementById('botao-enviar')
@@ -35,8 +35,8 @@ function enableSubmit() {
 
 
 window.onload = function() {
-  const botaoSubmit = document.getElementById('botao-enviar')
-  botaoSubmit.addEventListener('click', handleSubmit)
+  // const botaoSubmit = document.getElementById('botao-enviar')
+  // botaoSubmit.addEventListener('click', handleSubmit)
 
   const checkImagens = document.getElementById('check-2')
   checkImagens.addEventListener('change', enableSubmit)
@@ -50,3 +50,68 @@ document.getElementById('data').DatePickerX.init( {
   clearButton: false,
   format: 'dd/mm/yyyy',
 });
+
+
+const validate = new window.JustValidate('#form', 
+  {
+    focusInvalidField: true,
+    lockForm: true,
+    tooltip: {
+      position: 'top',
+    },
+  });
+
+validate
+  .addField('#nome-completo', [
+    {
+      rule: 'minLength',
+      value: 10,
+    },
+    {
+      rule: 'maxLength',
+      value: 40,
+    },
+    {
+      rule: 'required',
+      errorMessage: 'Full name is required'
+    },
+  ])
+
+  .addField('#e-mail', [
+    {
+      rule: 'minLength',
+      value: 10,
+    },
+    {
+      rule: 'maxLength',
+      value: 50,
+    },
+    {
+      rule: 'required',
+      errorMessage: 'Email is required',
+    },
+    {
+      rule: 'email',
+      errorMessage: 'Email is invalid!',
+    },
+  ])
+  
+  .addField('#text', [
+    {
+      rule: 'maxLength',
+      value: 500,
+    },
+    {
+      rule: 'required',
+      errorMessage: 'Field is required',
+    },
+  ])
+
+  .addField('#data', [
+    {
+      rule: 'required',
+      errorMessage: 'Field is required',
+    },
+  ])
+  
+  
