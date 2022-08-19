b23c6aabc6e7eb830e69759c8f86aa3951dd2981
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-08-19 13:17:46 -0300

    exercicio 3

--- fundamentos/B8-hofs-do-js-es6/8.1-js-es6-intro-a-hofs/exercicio.js
+++ fundamentos/B8-hofs-do-js-es6/8.1-js-es6-intro-a-hofs/exercicio.js
@@ -19,22 +19,63 @@
 
 // console.log(newEmployees(dados));
 
+
 // ==========EXERCICIO 2
-const checa = (apostado, sorteado) => {
-  let resultado = "Tente novamente..."
-  if (apostado === sorteado) {
-    resultado = "Parabéns, você ganhou!"
-  } 
-  return resultado
+// const checa = (apostado, sorteado) => {
+//   let resultado = "Tente novamente..."
+//   if (apostado === sorteado) {
+//     resultado = "Parabéns, você ganhou!"
+//   } 
+//   return resultado
+// };
+
+// const resultado = (apostado, callback) => {
+//   let sorteado = (Math.random() * 5) + 1;
+//   return callback(apostado, sorteado);
+// }
+
+// console.log(resultado(2, checa));
+
+// // Alternativa:
+// // const numberChecker = (myNumber, number) => myNumber === number;
+// // return callback(myNumber, number) ? 'Lucky day, you won!' : 'Try Again!';
+
+
+//========= EXERCICIO 3
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const comparaRespostas = (correta, marcada) => {
+  // let total = 0;
+  // for (let i = 0; i < correta.length; i += 1) {
+  //   if (correta[i] === marcada[i]) {
+  //     total += 1;
+  //   }
+  //   else if (correta[i] !== marcada[i] && marcada[i] !== 'N.A'){
+  //     total -= 0.5
+  //   }
+  // }
+  // return total
+
+  if (correta === marcada) {
+    return 1;
+  } if (marcada === 'N.A') {
+    return 0;
+  }
+  return -0.5;
 };
 
-const resultado = (apostado, callback) => {
-  let sorteado = (Math.random() * 5) + 1;
-  return callback(apostado, sorteado);
-}
+const totalPontos = (correta, marcada, callback) => {
+  // return callback(correta, marcada)
 
-console.log(resultado(2, checa));
+  let counter = 0;
+  for (let i = 0; i < correta.length; i += 1) {
+    const callbackReturn = callback(correta[i], marcada[i]);
+    counter += callbackReturn;
+  }
+  return `Resultado final: ${counter} pontos`;
+};
 
-// Alternativa:
-// const numberChecker = (myNumber, number) => myNumber === number;
-// return callback(myNumber, number) ? 'Lucky day, you won!' : 'Try Again!';
+console.log(totalPontos(RIGHT_ANSWERS, STUDENT_ANSWERS, comparaRespostas));
+
+// essa segunda maneira é do gabarito. A minha funcionou, mas basicamente tava td na primeira função, a segunda só chamava. Depois, ficou apenas a comparação numa função, e soma de valores na segunda.
