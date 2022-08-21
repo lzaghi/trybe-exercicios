a747933509bf876dddc9e045695ee85c1413be0c
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-08-21 11:49:55 -0300

    bonus parte 2

--- fundamentos/B8-hofs-do-js-es6/8.1-js-es6-intro-a-hofs/bônus.js
+++ fundamentos/B8-hofs-do-js-es6/8.1-js-es6-intro-a-hofs/bônus.js
@@ -26,23 +26,23 @@ const battleMembers = { mage, warrior, dragon };
 const danoDragao = () => {
   const danoMax = dragon.strength;
   const danoMin = 15;
-  return Math.floor(Math.random() * (danoMax - danoMin) + danoMin)
+  return Math.floor(Math.random() * (danoMax - danoMin + 1) + danoMin)
 }
-console.log(danoDragao());
+// console.log(danoDragao());
 
 // EXERCICIO 2
 const danoWarrior = () => {
   const danoMax = warrior.strength * warrior.weaponDmg;
   const danoMin = warrior.strength;
-  return Math.floor(Math.random() * (danoMax - danoMin) + danoMin)
+  return Math.floor(Math.random() * (danoMax - danoMin + 1) + danoMin)
 }
-console.log(danoWarrior());
+// console.log(danoWarrior());
 
 // EXERCICIO 3
 const ataqueMage = () => {
   const danoMax = mage.intelligence * 2;
   const danoMin = mage.intelligence;
-  const danoMage = Math.floor(Math.random() * (danoMax - danoMin) + danoMin)
+  const danoMage = Math.floor(Math.random() * (danoMax - danoMin + 1) + danoMin)
 
   const dadosTurno = {
     manaGasta: 0,
@@ -58,4 +58,29 @@ const ataqueMage = () => {
 
   return dadosTurno;
 }
-console.log(ataqueMage());
+// console.log(ataqueMage());
+
+
+// =============== PARTE 2
+const gameActions = {
+  turnoWarrior: (danoWarrior) => {
+    dragon.healthPoints -= danoWarrior;
+    warrior.damage = danoWarrior;
+  },
+  turnoMage: (ataqueMage) => {
+    dragon.healthPoints -= ataqueMage.dano;
+    mage.damage = ataqueMage.dano;
+    mage.mana -= ataqueMage.manaGasta;
+  },
+  turnoDragon: (danoDragao) => {
+    warrior.healthPoints -= danoDragao;
+    mage.healthPoints -= danoDragao;
+    dragon.damage = danoDragao;
+  },
+  resultTurno: () => battleMembers,
+};
+
+gameActions.turnoWarrior(danoWarrior());
+gameActions.turnoMage(ataqueMage());
+gameActions.turnoDragon(danoDragao());
+console.log(gameActions.resultTurno());
