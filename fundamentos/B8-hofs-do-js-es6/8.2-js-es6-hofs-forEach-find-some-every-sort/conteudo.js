ee8d7121b99a6de36bb9aaab80b973e0b0870e70
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-08-22 15:40:03 -0300

    conteudo some-every: 2

--- fundamentos/B8-hofs-do-js-es6/8.2-js-es6-hofs-forEach-find-some-every-sort/conteudo.js
+++ fundamentos/B8-hofs-do-js-es6/8.2-js-es6-hofs-forEach-find-some-every-sort/conteudo.js
@@ -43,8 +43,21 @@
 
 
 // =========== SOME E EVERY
-const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+// const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
 
-const hasName = (arr, name) => arr.some((elem) => elem === name)
+// const hasName = (arr, name) => arr.some((elem) => elem === name)
 
-console.log(hasName(names, 'Ana'))
+// console.log(hasName(names, 'Ana'))
+
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => arr.every((elem) => elem.age >= minimumAge)
+
+console.log(verifyAges(people, 10));
