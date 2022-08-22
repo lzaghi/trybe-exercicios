d78d983c0167cb3be5229403485b2d42b1e48d17
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-08-22 15:43:13 -0300

    conteudo sort: 1

--- fundamentos/B8-hofs-do-js-es6/8.2-js-es6-hofs-forEach-find-some-every-sort/conteudo.js
+++ fundamentos/B8-hofs-do-js-es6/8.2-js-es6-hofs-forEach-find-some-every-sort/conteudo.js
@@ -50,6 +50,20 @@
 // console.log(hasName(names, 'Ana'))
 
 
+// const people = [
+//   { name: 'Mateus', age: 18 },
+//   { name: 'José', age: 16 },
+//   { name: 'Ana', age: 23 },
+//   { name: 'Cláudia', age: 20 },
+//   { name: 'Bruna', age: 19 },
+// ];
+
+// const verifyAges = (arr, minimumAge) => arr.every((elem) => elem.age >= minimumAge)
+
+// console.log(verifyAges(people, 10));
+
+
+// ======== SORT
 const people = [
   { name: 'Mateus', age: 18 },
   { name: 'José', age: 16 },
@@ -58,6 +72,6 @@ const people = [
   { name: 'Bruna', age: 19 },
 ];
 
-const verifyAges = (arr, minimumAge) => arr.every((elem) => elem.age >= minimumAge)
+people.sort((a, b) => a.age - b.age)
 
-console.log(verifyAges(people, 10));
+console.log(people);
