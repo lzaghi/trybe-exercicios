92057fd82fe2f4056a916d2c0b82e68fe20dccaa
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-08-22 15:23:12 -0300

    conteudo find: 1

--- fundamentos/B8-hofs-do-js-es6/8.2-js-es6-hofs-forEach-find-some-every-sort/conteudo.js
+++ fundamentos/B8-hofs-do-js-es6/8.2-js-es6-hofs-forEach-find-some-every-sort/conteudo.js
@@ -1,13 +1,22 @@
 // ========= FOR EACH
-const emailListInData = [
-  'roberta@email.com',
-  'paulo@email.com',
-  'anaroberta@email.com',
-  'fabiano@email.com',
-];
+// const emailListInData = [
+//   'roberta@email.com',
+//   'paulo@email.com',
+//   'anaroberta@email.com',
+//   'fabiano@email.com',
+// ];
 
-const showEmailList = (email) => {
-  console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
-};
+// const showEmailList = (email) => {
+//   console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+// };
 
-emailListInData.forEach((element) => showEmailList(element))
+// emailListInData.forEach((element) => showEmailList(element))
+// // emailListInData.forEach(showEmailList);      JA DAVA!!!
+ 
+
+// ================= FIND
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = () => numbers.find((element) => element % 3 === 0 && element % 5 ===0)
+
+console.log(findDivisibleBy3And5())
