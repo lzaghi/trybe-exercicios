d5fe98b36f967ca2dba5159f00312efc785921bd
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-08-22 15:46:11 -0300

    conteudo sort: 2

--- fundamentos/B8-hofs-do-js-es6/8.2-js-es6-hofs-forEach-find-some-every-sort/conteudo.js
+++ fundamentos/B8-hofs-do-js-es6/8.2-js-es6-hofs-forEach-find-some-every-sort/conteudo.js
@@ -64,6 +64,19 @@
 
 
 // ======== SORT
+// const people = [
+//   { name: 'Mateus', age: 18 },
+//   { name: 'José', age: 16 },
+//   { name: 'Ana', age: 23 },
+//   { name: 'Cláudia', age: 20 },
+//   { name: 'Bruna', age: 19 },
+// ];
+
+// people.sort((a, b) => a.age - b.age)
+
+// console.log(people);
+
+
 const people = [
   { name: 'Mateus', age: 18 },
   { name: 'José', age: 16 },
@@ -72,6 +85,6 @@ const people = [
   { name: 'Bruna', age: 19 },
 ];
 
-people.sort((a, b) => a.age - b.age)
+people.sort((a, b) => b.age - a.age)
 
 console.log(people);
