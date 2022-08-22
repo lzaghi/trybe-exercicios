8e0124c3ac30b95a8455c5b8e8dc8de45302459f
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-08-22 15:35:44 -0300

    conteudo find: 3

--- fundamentos/B8-hofs-do-js-es6/8.2-js-es6-hofs-forEach-find-some-every-sort/conteudo.js
+++ fundamentos/B8-hofs-do-js-es6/8.2-js-es6-hofs-forEach-find-some-every-sort/conteudo.js
@@ -22,8 +22,21 @@
 // console.log(findDivisibleBy3And5())
 
 
-const names = ['João', 'Irene', 'Fernando', 'Maria'];
+// const names = ['João', 'Irene', 'Fernando', 'Maria'];
 
-const findNameWithFiveLetters = () => names.find((name) => name.length === 5)
+// const findNameWithFiveLetters = () => names.find((name) => name.length === 5)
 
-console.log(findNameWithFiveLetters());
+// console.log(findNameWithFiveLetters());
+
+
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+]
+
+function findMusic(id) {
+ return musicas.find((element) => element.id === id)
+}
+
+console.log(findMusic('31031685'))
