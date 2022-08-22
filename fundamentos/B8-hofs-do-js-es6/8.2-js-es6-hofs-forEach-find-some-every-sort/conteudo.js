ea60825867da4ade2cff4634719d5e07dfde039b
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-08-22 15:37:57 -0300

    conteudo some-every: 1

--- fundamentos/B8-hofs-do-js-es6/8.2-js-es6-hofs-forEach-find-some-every-sort/conteudo.js
+++ fundamentos/B8-hofs-do-js-es6/8.2-js-es6-hofs-forEach-find-some-every-sort/conteudo.js
@@ -29,14 +29,22 @@
 // console.log(findNameWithFiveLetters());
 
 
-const musicas = [
-  { id: '31031685', title: 'Partita in C moll BWV 997' },
-  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
-  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
-]
+// const musicas = [
+//   { id: '31031685', title: 'Partita in C moll BWV 997' },
+//   { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+//   { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+// ]
 
-function findMusic(id) {
- return musicas.find((element) => element.id === id)
-}
+// function findMusic(id) {
+//  return musicas.find((element) => element.id === id)
+// }
 
-console.log(findMusic('31031685'))
+// console.log(findMusic('31031685'))
+
+
+// =========== SOME E EVERY
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => arr.some((elem) => elem === name)
+
+console.log(hasName(names, 'Ana'))
