b61f9f4c93602d0f4f0a804dc78158bef7b1daf6
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-08-24 15:05:16 -0300

    conteudo reduce: 1 gabarito

--- fundamentos/B8-hofs-do-js-es6/8.4-js-es6-hofs-reduce/conteudo.js
+++ fundamentos/B8-hofs-do-js-es6/8.4-js-es6-hofs-reduce/conteudo.js
@@ -1,5 +1,10 @@
 // buscar maior valor em um array
 const numbers = [50, 85, -30, 3, 15];
 
-const maior = numbers.reduce((acc, curr) => curr > acc? acc = curr : acc = acc);
-console.log(maior);
+// const maior = numbers.reduce((acc, curr) => curr > acc? acc = curr : acc = acc);
+// console.log(maior);
+
+const getBigger = (bigger, number) => ((bigger > number) ? bigger : number);
+
+const bigger = numbers.reduce(getBigger, 0);
+console.log(bigger); // 85
