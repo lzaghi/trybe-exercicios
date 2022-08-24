a0c8e9b5c6077b6e56d935f2d8746e81c5870785
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-08-24 15:22:06 -0300

    conteudo reduce: 2 gabarito alternativo

--- fundamentos/B8-hofs-do-js-es6/8.4-js-es6-hofs-reduce/conteudo.js
+++ fundamentos/B8-hofs-do-js-es6/8.4-js-es6-hofs-reduce/conteudo.js
@@ -18,9 +18,19 @@ const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
 
 // console.log(somaPares);
 
-const getEven = (number) => number % 2 === 0;
-const sumPair = (accumulator, number) => accumulator + number;
+//gabarito
+// const getEven = (number) => number % 2 === 0;
+// const sumPair = (accumulator, number) => accumulator + number;
 
-const sumNumbers = (array) => array.filter(getEven).reduce(sumPair); // Olhe que código pequeno e conciso!
+// const sumNumbers = (array) => array.filter(getEven).reduce(sumPair); // Olhe que código pequeno e conciso!
+
+// console.log(sumNumbers(numbers)); // 152
+
+//gabarito alternativo
+const sumPair = (accumulator, number) => (
+  (number % 2 === 0) ? accumulator + number : accumulator
+);
+
+const sumNumbers = (array) => array.reduce(sumPair, 0);
 
 console.log(sumNumbers(numbers)); // 152
