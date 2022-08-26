2c2623c9e83c2da63354e92dfc31f5c5cd5919e0
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-08-26 10:29:38 -0300

    conteudo obj destructuring

--- fundamentos/B8-hofs-do-js-es6/8.5-js-es6-spread-rest-destructuring-+/conteudo.js
+++ fundamentos/B8-hofs-do-js-es6/8.5-js-es6-spread-rest-destructuring-+/conteudo.js
@@ -1,4 +1,4 @@
-//SPREAD
+//===============SPREAD
 // Faça uma lista com as suas frutas favoritas
 const specialFruit = ['abacaxi', 'uva', 'larnaja', 'morango'];
 
@@ -10,4 +10,24 @@ const fruitSalad = (fruit, additional) => {
   return pedido
 }
 
-console.log(fruitSalad(specialFruit, additionalItens));
+// console.log(fruitSalad(specialFruit, additionalItens));
+
+
+//============OBJECT DESTRUCTURING
+const user = {
+  name: 'Maria',
+  age: 21,
+  nationality: 'Brazilian',
+};
+
+const jobInfos = {
+  profession: 'Software engineer',
+  squad: 'Rocket Landing Logic',
+  squadInitials: 'RLL',
+};
+
+const infosCompletas = {...user, ...jobInfos}
+console.log(infosCompletas);
+
+const { name, age, nationality, profession, squad, squadInitials } = infosCompletas
+console.log(`Hi, my name is ${name}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad} `);
