48128421e39eb64be187f078fdf5297388d7ac90
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-08-26 10:49:50 -0300

    conteudo default parameters

--- fundamentos/B8-hofs-do-js-es6/8.5-js-es6-spread-rest-destructuring-+/conteudo.js
+++ fundamentos/B8-hofs-do-js-es6/8.5-js-es6-spread-rest-destructuring-+/conteudo.js
@@ -88,4 +88,12 @@ const getPosition = (latitude, longitude) => ({
   latitude,
   longitude,});
 
-console.log(getPosition(-19.8157, -43.9542));
+// console.log(getPosition(-19.8157, -43.9542));
+
+
+// =========== DEFAULT PARAMETERS
+const multiply = (number, value = 1) => {
+  return number * value
+};
+
+console.log(multiply(8));
