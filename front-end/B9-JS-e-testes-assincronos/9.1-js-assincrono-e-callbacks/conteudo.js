4adc768d541ec5fff90f1ddb5b709299a59e751d
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-09-07 13:40:09 -0300

    conteudo callbacks: 2

--- front-end/B9-JS-e-testes-assincronos/9.1-js-assincrono-e-callbacks/conteudo.js
+++ front-end/B9-JS-e-testes-assincronos/9.1-js-assincrono-e-callbacks/conteudo.js
@@ -37,17 +37,41 @@
 // despesaMensal(minhaRenda, minhasDespesas, somaDespesas);
 
 //================== CALLBACKS
+// const userFullName = ({ firstName, lastName }) => `Olá! Meu nome é ${firstName} ${lastName}`;
+// const userNationality = ({ firstName, nationality }) => `${firstName} é ${nationality}`;
+
+// const getUser = (callback) => {
+//   const user = {
+//     firstName: 'Ivan',
+//     lastName: 'Ivanovich',
+//     nationality: 'Russo',
+//   };
+//   return callback(user)
+// };
+
+// console.log(getUser(userFullName)); // Retorno esperado: "Olá! Meu nome é Ivan Ivanovich"
+// console.log(getUser(userNationality)); // Retorno esperado: "Ivan é Russo"
+
+
 const userFullName = ({ firstName, lastName }) => `Olá! Meu nome é ${firstName} ${lastName}`;
 const userNationality = ({ firstName, nationality }) => `${firstName} é ${nationality}`;
 
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
 const getUser = (callback) => {
-  const user = {
-    firstName: 'Ivan',
-    lastName: 'Ivanovich',
-    nationality: 'Russo',
-  };
-  return callback(user)
+  setTimeout(() => {
+    const user = {
+      firstName: 'Ivan',
+      lastName: 'Ivanovich',
+      nationality: 'Russo',
+    };
+
+    // Dica: use esse `console.log()` abaixo para imprimir o resultado na tela.
+    console.log(callback(user));
+  }, delay());
 };
 
-console.log(getUser(userFullName)); // Retorno esperado: "Olá! Meu nome é Ivan Ivanovich"
-console.log(getUser(userNationality)); // Retorno esperado: "Ivan é Russo"
+getUser(userFullName); // deve imprimir "Olá! Meu nome é Ivan Ivanovich" depois de um certo tempo
+getUser(userNationality); // deve imprimir "Ivan é Russo" depois de um certo tempo
+
+//momentos em q nome da pessoa imprime primeiro, outros em q nacionalidade imprime primeiro!!!
