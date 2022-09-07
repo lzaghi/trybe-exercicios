daafb9f996c17df19c3f701c36eb573ccd3c8503
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-09-07 13:54:35 -0300

    exercicio 3

--- front-end/B9-JS-e-testes-assincronos/9.1-js-assincrono-e-callbacks/exercicios.js
+++ front-end/B9-JS-e-testes-assincronos/9.1-js-assincrono-e-callbacks/exercicios.js
@@ -34,37 +34,54 @@
 // // ORDEM DE RETORNO : A B C
 
 
-// =============== EXERCICIO 2
-const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
-  `${name} está a ${value} ${measurementUnit} de distância do Sol`;
+// // =============== EXERCICIO 2
+// const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+//   `${name} está a ${value} ${measurementUnit} de distância do Sol`;
 
-const MEASUREMENT_UNIT = 'quilômetros';
+// const MEASUREMENT_UNIT = 'quilômetros';
 
-const mars = {
-  name: 'Marte',
-  distanceFromSun: {
-    value: 227900000,
-    measurementUnit: MEASUREMENT_UNIT,
-  },
+// const mars = {
+//   name: 'Marte',
+//   distanceFromSun: {
+//     value: 227900000,
+//     measurementUnit: MEASUREMENT_UNIT,
+//   },
+// };
+
+// const venus = {
+//   name: 'Venus',
+//   distanceFromSun: {
+//     value: 108200000,
+//     measurementUnit: MEASUREMENT_UNIT,
+//   },
+// };
+
+// const jupiter = {
+//   name: 'Jupiter',
+//   distanceFromSun: {
+//     value: 778500000,
+//     measurementUnit: MEASUREMENT_UNIT,
+//   },
+// };
+
+// console.log(planetDistanceFromSun(mars)); // A
+// setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
+// setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
+// // ORDEM DE RETORNO : A C B
+
+
+// =============== EXERCICIO 3
+const getPlanet = () => {
+  setTimeout(() => {
+    const mars = {
+      name: 'Mars',
+      distanceFromSun: {
+        value: 227900000,
+        measurementUnit: 'kilometers',
+      },
+    };
+    console.log('Returned planet: ', mars);
+  }, 4000)
 };
 
-const venus = {
-  name: 'Venus',
-  distanceFromSun: {
-    value: 108200000,
-    measurementUnit: MEASUREMENT_UNIT,
-  },
-};
-
-const jupiter = {
-  name: 'Jupiter',
-  distanceFromSun: {
-    value: 778500000,
-    measurementUnit: MEASUREMENT_UNIT,
-  },
-};
-
-console.log(planetDistanceFromSun(mars)); // A
-setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
-setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
-// ORDEM DE RETORNO : A C B
+getPlanet(); // Imprime Marte depois de 4 segundos
