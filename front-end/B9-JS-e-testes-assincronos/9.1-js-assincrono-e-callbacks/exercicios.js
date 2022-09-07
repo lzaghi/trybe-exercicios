f96bfb0b05f86ee715382e8f489c1e3384d8fddc
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-09-07 14:36:24 -0300

    exercicio 7

--- front-end/B9-JS-e-testes-assincronos/9.1-js-assincrono-e-callbacks/exercicios.js
+++ front-end/B9-JS-e-testes-assincronos/9.1-js-assincrono-e-callbacks/exercicios.js
@@ -133,41 +133,84 @@
 
 
 // =============== EXERCICIO 6
-const messageDelay = () => Math.floor(Math.random() * 5000);
+// const messageDelay = () => Math.floor(Math.random() * 5000);
 
-const getMarsTemperature = () => {
-  const maxTemperature = 58;
-  return Math.floor(Math.random() * maxTemperature);
-}
+// const getMarsTemperature = () => {
+//   const maxTemperature = 58;
+//   return Math.floor(Math.random() * maxTemperature);
+// }
 
-const toFahrenheit = (degreeCelsius) => (degreeCelsius * (9 / 5)) + 32;
+// const toFahrenheit = (degreeCelsius) => (degreeCelsius * (9 / 5)) + 32;
 
-const temperatureInFahrenheit = (temperature) =>
-  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+// const temperatureInFahrenheit = (temperature) =>
+//   console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
 
-const greet = (temperature) =>
-  console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+// const greet = (temperature) =>
+//   console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
 
-const handleError = (errorReason) =>
-  console.log(`Error getting temperature: ${errorReason}`);
+// const handleError = (errorReason) =>
+//   console.log(`Error getting temperature: ${errorReason}`);
 
-const sendMarsTemperature = (onSuccess, onError) => {
-  const currentTemperature = getMarsTemperature();
-  const errorMessage = 'Robot is busy';
+// const sendMarsTemperature = (onSuccess, onError) => {
+//   const currentTemperature = getMarsTemperature();
+//   const errorMessage = 'Robot is busy';
 
-  const happenChance = Math.random() <= 0.6;
+//   const happenChance = Math.random() <= 0.6;
   
+//   setTimeout(() => {
+//     if(happenChance) {
+//       onSuccess(currentTemperature);
+//     } else {
+//       onError(errorMessage);
+//     }
+//   }, messageDelay())
+// }
+
+// // imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+// sendMarsTemperature(temperatureInFahrenheit, handleError);
+
+// // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+// sendMarsTemperature(greet, handleError);
+
+
+// ================= EXERCICIO 7
+const pokemons = [
+  {
+    name: 'Bulbasaur',
+    type: 'Grama',
+    ability: 'Raio Solar',
+  },
+  {
+    name: 'Charmander',
+    type: 'Fogo',
+    ability: 'Lança Chamas',
+  },
+  {
+    name: 'Squirtle',
+    type: 'Água',
+    ability: 'Jato de Água',
+  },
+];
+
+function getPokemonDetails(selectedPokemon, callback) {
+  const foundPokemon = pokemons.find((pokemon) => pokemon.name === selectedPokemon);
+
   setTimeout(() => {
-    if(happenChance) {
-      onSuccess(currentTemperature);
-    } else {
-      onError(errorMessage);
+    if (foundPokemon === undefined) {
+      return callback(new Error('Não temos esse pokémon para você :('), null);
     }
-  }, messageDelay())
+
+    const { name, type, ability } = foundPokemon;
+
+    const messageFromProfOak = `Olá, seu pokémon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}`;
+
+    callback(null, messageFromProfOak);
+  }, 2000);
 }
 
-// imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
-sendMarsTemperature(temperatureInFahrenheit, handleError);
+const handlePokemonSearch = (error, message) => {
+  if (error !== null) console.log(error);      //só if(error) tb da!!!!
+  else console.log(message);
+};
 
-// imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
-sendMarsTemperature(greet, handleError);
+getPokemonDetails('Charmander', handlePokemonSearch);
