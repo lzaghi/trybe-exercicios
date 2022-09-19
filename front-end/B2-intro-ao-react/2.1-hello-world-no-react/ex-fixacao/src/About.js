2e767039df04d683fab390ff5b96e742a919a448
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-09-19 19:48:19 -0300

    exercicios fixacao

---
+++ front-end/B2-intro-ao-react/2.1-hello-world-no-react/ex-fixacao/src/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Lucca</h1>
+        <p>Aluno Trybe</p>
+
+        <h2>Minhas Habilidades</h2>
+        <ul>
+          <li>html</li>
+          <li>css</li>
+          <li>js</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default About;
