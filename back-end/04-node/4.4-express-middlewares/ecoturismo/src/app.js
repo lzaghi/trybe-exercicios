1d0fbf86f519653c53a4fb7ac8f863687a7332f9
Lucca Zaghi <lucca.zaghi@gmail.com>
2023-02-06 12:25:31 -0300

    exercicio 1

--- back-end/04-node/4.4-express-middlewares/ecoturismo/src/app.js
+++ back-end/04-node/4.4-express-middlewares/ecoturismo/src/app.js
@@ -1,8 +1,26 @@
 // app.js
 const express = require('express');
+const validateName = require('./middlewares/validateName');
+const validatePrice = require('./middlewares/validatePrice');
+const validateDescription = require('./middlewares/validateDescription');
+const validateCreatedAt = require('./middlewares/validateCreatedAt');
+const validateRating = require('./middlewares/validateRating');
+const validateDifficulty = require('./middlewares/validateDifficulty');
 
 const app = express();
 
 app.use(express.json());
+
+app.post(
+  '/activities',
+  validateName,
+  validatePrice,
+  validateDescription,
+  validateCreatedAt,
+  validateRating,
+  validateDifficulty,
+  (_req, res) => {
+  res.status(201).json({ message: 'Atividade registrada com sucesso!' });
+});
 
 module.exports = app;
