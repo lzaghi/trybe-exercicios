0b614e67d27b3b2a25f43206203a784dce15add8
Lucca Zaghi <lucca.zaghi@gmail.com>
2023-02-06 12:35:26 -0300

    exercicio 2

--- back-end/04-node/4.4-express-middlewares/ecoturismo/src/app.js
+++ back-end/04-node/4.4-express-middlewares/ecoturismo/src/app.js
@@ -6,6 +6,8 @@ const validateDescription = require('./middlewares/validateDescription');
 const validateCreatedAt = require('./middlewares/validateCreatedAt');
 const validateRating = require('./middlewares/validateRating');
 const validateDifficulty = require('./middlewares/validateDifficulty');
+const generateToken = require('./utils/generateToken');
+const auth = require('./middlewares/auth');
 
 const app = express();
 
@@ -13,6 +15,7 @@ app.use(express.json());
 
 app.post(
   '/activities',
+  auth,
   validateName,
   validatePrice,
   validateDescription,
@@ -21,6 +24,17 @@ app.post(
   validateDifficulty,
   (_req, res) => {
   res.status(201).json({ message: 'Atividade registrada com sucesso!' });
+});
+
+app.post('/signup', (req, res) => {
+  const { email, password, firstName, phone } = req.body;
+
+  if ([email, password, firstName, phone].includes(undefined)) {
+    return res.status(401).json({ message: 'Campos ausentes!' });
+  }
+
+  const token = generateToken();
+  return res.status(200).json({ token });
 });
 
 module.exports = app;
