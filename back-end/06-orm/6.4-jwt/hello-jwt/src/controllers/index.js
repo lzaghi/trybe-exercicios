078c24ec056f2b4f8640bdd5c5eabc486d87a306
Lucca Zaghi <lucca.zaghi@gmail.com>
2023-03-03 14:57:57 -0300

    exercicios 6.4

---
+++ back-end/06-orm/6.4-jwt/hello-jwt/src/controllers/index.js
@@ -0,0 +1,13 @@
+const me = require('./me');
+const ping = require('./ping');
+const login = require('./login');
+const topSecret = require('./topSecret');
+const signup = require('./signup');
+
+module.exports = {
+  me,
+  ping,
+  login,
+  signup,
+  topSecret,
+};
